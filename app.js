bfd98bfbd09eb7e13f11e3d2a7210f85139aff49
Joan Gavelán <joangavelan@gmail.com>
2021-09-23 21:49:06 -0500

    generic types

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 "use strict";
+var names = ["Max", "Manu"];
+// names[0].split(' ');
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(10);
+    }, 2000);
+});
+promise.then(function (data) {
+    // data.split(' ')
+});

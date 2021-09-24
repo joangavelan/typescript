3a727d0d13cc3c6d40eae4c8f3bc6c7a456d7c77
Joan Gavelán <joangavelan@gmail.com>
2021-09-23 22:14:31 -0500

    creating a generic function

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 "use strict";
-var names = ["Max", "Manu"];
-// names[0].split(' ');
-var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve(10);
-    }, 2000);
-});
-promise.then(function (data) {
-    // data.split(' ')
-});
+// const names = ["Max", "Manu"];
+// // names[0].split(' ');
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+// promise.then(data => {
+//   // data.split(' ')
+// })
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
+}
+var mergedObjects = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObjects.name);
--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-const names = ["Max", "Manu"];
-// names[0].split(' ');
+// const names = ["Max", "Manu"];
+// // names[0].split(' ');
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then(data => {
-  // data.split(' ')
-})
+// promise.then(data => {
+//   // data.split(' ')
+// })
+
+function merge<T, U>(obj1: T, obj2: U) {
+  return Object.assign(obj1, obj2);
+}
+
+const mergedObjects = merge({ name: "Max" }, { age: 30 });
+
+console.log(mergedObjects.name);

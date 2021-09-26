ee497a7a15ad93f678d32e544e52c62ed8ddb547
Joan Gavelán <joangavelan@gmail.com>
2021-09-26 08:31:04 -0500

    generic classes

--- app.ts
+++ app.ts
@@ -19,25 +19,72 @@
 
 // console.log(mergedObjects);
 
-interface Lengthy {
-  length: number;
+// interface Lengthy {
+//   length: number;
+// }
+
+// function countAndDescribe<T extends Lengthy>(elem: T): [T, string] {
+//   let descriptionText = "Got no value.";
+//   if (elem.length === 1) descriptionText = "Got 1 element";
+//   else if (elem.length > 0)
+//     descriptionText = "Got " + elem.length + " elements.";
+//   return [elem, descriptionText];
+// }
+
+// console.log(countAndDescribe("Hi there!"));
+
+// function extractAndConvert<T extends object, U extends keyof T>(
+//   obj: T,
+//   key: U
+// ) {
+//   return obj[key];
+// }
+
+// console.log(extractAndConvert({ name: "Joan" }, "name"));
+
+class DataStorage<T extends string | number> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if(this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1);
+    // console.log(this.data.indexOf(item));
+  }
+
+  getItems() {
+    return [...this.data];
+  }
 }
 
-function countAndDescribe<T extends Lengthy>(elem: T): [T, string] {
-  let descriptionText = "Got no value.";
-  if (elem.length === 1) descriptionText = "Got 1 element";
-  else if (elem.length > 0)
-    descriptionText = "Got " + elem.length + " elements.";
-  return [elem, descriptionText];
-}
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+const textDataStoraged = textStorage.getItems();
+textDataStoraged.push("hey");
 
-console.log(countAndDescribe("Hi there!"));
+console.log(textStorage.getItems());
+console.log(textDataStoraged);
 
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return obj[key];
-}
+const numbersStorage = new DataStorage<number>();
+numbersStorage.addItem(1);
+numbersStorage.addItem(2);
+numbersStorage.removeItem(2);
+console.log(numbersStorage.getItems());
 
-console.log(extractAndConvert({ name: "Joan" }, "name"));
+// const objStorage = new DataStorage<object>();
+
+// const maxObjRef = { name: "Max "};
+
+// objStorage.addItem(maxObjRef);
+// objStorage.addItem({ name: "Joan" });
+// objStorage.addItem({ name: "Manu" });
+// // ...
+
+// objStorage.removeItem(maxObjRef)
+
+// console.log(objStorage.getItems());

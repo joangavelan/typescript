f3a512595bac9e373ad68d823f2b5a0c58592a5e
Joan Gavelán <joangavelan@gmail.com>
2021-10-02 00:44:06 -0500

    diving into property decorators

--- app.js
+++ app.js
@@ -1,38 +1,58 @@
 "use strict";
+// function Logger(logString: string) {
+//   console.log("LOGGER FACTORY");
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(logString) {
-    console.log('LOGGER FACTORY');
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-}
-function WithTemplate(template, hookId) {
-    console.log('TEMPLATE FACTORY');
-    return function (constructor) {
-        console.log('Rendering template');
-        const HTMLelem = document.getElementById(hookId);
-        const p = new constructor();
-        if (HTMLelem) {
-            HTMLelem.innerHTML = template;
-            HTMLelem.querySelector('h1').textContent = p.name;
-        }
-    };
-}
-let Person = class Person {
-    constructor() {
-        this.name = 'Joan';
-        console.log("Creating person object...");
-    }
-};
-Person = __decorate([
-    Logger('LOGGING'),
-    WithTemplate("<h1>My Person Object</h1>", "app")
-], Person);
+// function WithTemplate(template: string, hookId: string) {
+//   console.log("TEMPLATE FACTORY");
+//   return function (constructor: any) {
+//     console.log("Rendering template");
+//     const HTMLelem = document.getElementById(hookId);
+//     const p = new constructor();
+//     if (HTMLelem) {
+//       HTMLelem.innerHTML = template;
+//       HTMLelem.querySelector("h1")!.textContent = p.name;
+//     }
+//   };
+// }
+// @Logger("LOGGING")
+// @WithTemplate("<h1>My Person Object</h1>", "app")
+// class Person {
+//   name = "Joan";
+//   constructor() {
+//     console.log("Creating person object...");
+//   }
+// }
 // const pers = new Person();
 // console.log(pers);
+function Log(target, properyName) {
+    console.log('Propery decorator!');
+    console.log(target, properyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0)
+            this._price = val;
+        else
+            throw new Error("Invalid price - should be positive");
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
--- app.ts
+++ app.ts
@@ -1,35 +1,60 @@
-function Logger(logString: string) {
-  console.log('LOGGER FACTORY')
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
+// function Logger(logString: string) {
+//   console.log("LOGGER FACTORY");
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
 
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY')
-  return function (constructor: any) {
-    console.log('Rendering template')
-    const HTMLelem = document.getElementById(hookId);
-    const p = new constructor();
+// function WithTemplate(template: string, hookId: string) {
+//   console.log("TEMPLATE FACTORY");
+//   return function (constructor: any) {
+//     console.log("Rendering template");
+//     const HTMLelem = document.getElementById(hookId);
+//     const p = new constructor();
 
-    if (HTMLelem) {
-      HTMLelem.innerHTML = template;
-      HTMLelem.querySelector('h1')!.textContent = p.name;
-    }
-  };
-}
+//     if (HTMLelem) {
+//       HTMLelem.innerHTML = template;
+//       HTMLelem.querySelector("h1")!.textContent = p.name;
+//     }
+//   };
+// }
 
-@Logger('LOGGING')
-@WithTemplate("<h1>My Person Object</h1>", "app")
-class Person {
-  name = 'Joan';
+// @Logger("LOGGING")
+// @WithTemplate("<h1>My Person Object</h1>", "app")
+// class Person {
+//   name = "Joan";
 
-  constructor() {
-    console.log("Creating person object...");
-  }
-}
+//   constructor() {
+//     console.log("Creating person object...");
+//   }
+// }
 
 // const pers = new Person();
 
 // console.log(pers);
+
+function Log(target: any, properyName: string | Symbol) {
+  console.log('Propery decorator!');
+  console.log(target, properyName);
+} 
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) this._price = val;
+    else throw new Error("Invalid price - should be positive");
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
